6451ee167d231e2f495236c256ad48d21d722caa
RitaRawhy <rita.2000489@gmail.com>
2023-07-15 16:00:46 +0300

    add customer and customer banded

---
+++ Infrastructure/AppServices/SearchCustomerService.cs
@@ -0,0 +1,47 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Infrastructure.AppServices
+{
+    public class SearchCustomerService : ISearchCustomerService
+    {
+        private readonly LibraryDbContext _context;
+        private readonly IMapper _mapper;
+
+        public SearchCustomerService(LibraryDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IReadOnlyList<ReadCustomerDto>> SearchWithCriteria(string Name = null, string PhoneNumber = null)
+        {
+            var query = _context.Set<Customer>().AsQueryable();
+
+            if (!string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(PhoneNumber))
+            {
+                query = query.Where(A => A.CustomerName.Contains(Name) && A.CustomerPhoneNumber.Contains(PhoneNumber));
+            }
+            else if (!string.IsNullOrEmpty(Name))
+            {
+                query = query.Where(t => t.CustomerName.Contains(Name));
+            }
+            else if (!string.IsNullOrEmpty(PhoneNumber))
+            {
+                query = query.Where(t => t.CustomerPhoneNumber.Contains(PhoneNumber));
+            }
+            var result = await query.ToListAsync();
+            return _mapper.Map<IReadOnlyList<Customer>, IReadOnlyList<ReadCustomerDto>>(result);
+        }
+    }
+}
